5f18ae4ea42f29aea088b217ec2717bb70223cbd
slevinas <slevinas@gmail.com>
2024-09-23 11:00:31 -0400

    added nested routes and used the OUTLET tag and a default for no segment for parent component

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -17,9 +17,13 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login  />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Dashboard of List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
-
        
       </Routes> 
     </BrowserRouter>
