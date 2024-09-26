182cfc4af651bcfa43f6913238672098a2c1aa7e
slevinas <slevinas@gmail.com>
2024-09-26 08:49:40 -0400

    wrapping the components with the new Provider(context API)

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import City from './components/City'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import Form from './components/Form'
+import { CitiesProvider } from './contexts/CitiesContext'
 import AppLayout from './pages/AppLayout'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
@@ -18,23 +19,25 @@ function App() {
  
   
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login  />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to='cities' />} />
-          <Route path="cities" element={<CityList  />} />
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<CountryList  />} />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-       
-      </Routes> 
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login  />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to='cities' />} />
+            <Route path="cities" element={<CityList  />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList  />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        
+        </Routes> 
+      </BrowserRouter>
+    </CitiesProvider>
      
     
   )
