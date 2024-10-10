fe84254ecb66ff9278298abac11815af02b715df
slevinas <slevinas@gmail.com>
2024-10-10 12:23:52 -0400

    useCase showing the problematic nature of using useCallback and useEffect that is using the the func ints dipendency arrays

--- vite-base/src/Calculator.jsx
+++ vite-base/src/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import clickSound from './ClickSound.m4a'
 
 function Calculator({ workouts, allowSound }) {
@@ -6,19 +6,43 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
 
-
-
-  const playSound = function () {
+  const playSound = useCallback(
+    function () {
     if (!allowSound) return;
    
     const sound = new Audio(clickSound);
     sound.play();
-  };
+  },[allowSound]);
+
+
+  useEffect(
+    function (){
+      setDuration(
+        (number * sets * speed) / 60 + (sets - 1) * durationBreak);
+        playSound();
+    },
+    [number, sets, speed, durationBreak, playSound]
+  );
+
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+  function handleInc() {
+    setDuration((duration) => Math.floor(duration + 1));
+    playSound();
+  }
+
+  function handleDec() {
+    setDuration((duration) => Math.ceil(duration - 1));
+    playSound();
+  }
+
+
+ 
 
   return (
     <>
@@ -69,13 +93,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
