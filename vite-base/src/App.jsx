b9357cd3b2a094d5b6326ed75468d038a17f6cd8
slevinas <slevinas@gmail.com>
2024-10-11 14:12:26 -0400

    adding a removing file for new chapter on redux

--- vite-base/src/App.jsx
+++ vite-base/src/App.jsx
@@ -1,66 +1,17 @@
-import { useEffect, useMemo, useState } from "react"
-import Calculator from "./Calculator"
-import ToggleSounds from "./ToggleSounds"
-
-function formatTime(date) {
-  return new Intl.DateTimeFormat("en", {
-    month: "short",
-    year: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  }).format(date);
-}
+import AccountOperations from "./AccountOperations"
+import BalanceDisplay from "./BalanceDisplay"
+import CreateCustomer from "./CreateCustomer"
+import Customer from "./Customer"
 
 function App() {
-  const [allowSound, setAllowSound] = useState(true);
-  const [time, setTime] = useState(formatTime(new Date()));
-
-  // Will be be AM or PM
-  const partOfDay = time.slice(-2);
-
-  const workouts = useMemo(() => {
-    return [
-    {
-      name: "Full-body workout",
-      numExercises: partOfDay === "AM" ? 9 : 8,
-    },
-    {
-      name: "Arms + Legs",
-      numExercises: 6,
-    },
-    {
-      name: "Arms only",
-      numExercises: 3,
-    },
-    {
-      name: "Legs only",
-      numExercises: 4,
-    },
-    {
-      name: "Core only",
-      numExercises: partOfDay === "AM" ? 5 : 4,
-    },
-  ]
-}, [partOfDay]);
-
-
-
-  useEffect(function () {
-    const id = setInterval(function () {
-      setTime(formatTime(new Date()));
-    }, 1000);
-
-    return () => clearInterval(id);
-  }, []);
-
   return (
-    <main>
-      <h1>Workout timer</h1>
-      <time>For your workout on {time}</time>
-      <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
-      <Calculator workouts={workouts} allowSound={allowSound} />
-    </main>
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
+    </div>
   );
 }
 
